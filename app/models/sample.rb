0f76ea6bd2b865576b9085b5441f6490cf35699a
alexabbott <alex.r.abbott@gmail.com>
2014-12-05 11:19:44 -0800

    Initialize models

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,13 @@
 class Sample < ActiveRecord::Base
 
+	validates_uniqueness_of :name 
+
+	validates_length_of :name, maximum: 50
+
+	validates_uniqueness_of :url
+
+	validates_format_of :url, :with => URI::regexp(%w(http https))
+
+	validates_presence_of :category  
+
 end
--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
+require 'shoulda/matchers'
 
 describe Sample do
+
+	it { should validate_uniqueness_of(:name) }
+
+	it { should ensure_length_of(:name).is_at_most(50)}
+
+	it { should validate_uniqueness_of(:url) }
+
+	it { should validate_presence_of(:category) }
 
 end
