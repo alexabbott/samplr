5fd84526a8412cb4a45748920427be288fd2fc0b
alexabbott <alex.r.abbott@gmail.com>
2014-12-05 23:09:00 -0800

    Add main functionality for sample playing/switching

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,6 +1,14 @@
 class SamplesController < ApplicationController 
 	def index 
 		@samples = Sample.all
+		@kicks = Sample.where(category: 'kick')
+		@snares = Sample.where(category: 'snare')
+		@hihatsclosed = Sample.where(category: 'hi-hat closed')
+		@hihatsopen = Sample.where(category: 'hi-hat open')
+		@claps = Sample.where(category: 'clap')
+		@cymbals = Sample.where(category: 'cymbal')
+		@fx = Sample.where(category: 'fx')
+		@vocals = Sample.where(category: 'vocal')
 		@sample = Sample.new
 	end 
 
